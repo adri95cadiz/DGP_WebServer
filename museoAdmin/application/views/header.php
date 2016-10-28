4e7f66acfb36ab05d93312cac8e114fd877053df
GinnyDiaz <luceodet@gmail.com>
2016-10-28 13:52:03 +0200

    Cambios para prototipo 1

--- museoAdmin/application/views/header.php
+++ museoAdmin/application/views/header.php
@@ -348,96 +348,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                 </ul>
                                 <!-- /.nav-second-level -->
                             </li>
-                            <li>
-                                <a href="<?php echo base_url(); ?>index.php/admin/index"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/flot">Flot Charts</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/morris">Morris.js Charts</a>
-                                    </li>
-                                </ul>
-                                <!-- /.nav-second-level -->
-                            </li>
-                            <li>
-                                <a href="<?php echo base_url(); ?>index.php/admin/tables"><i class="fa fa-table fa-fw"></i> Tables</a>
-                            </li>
-                            <li>
-                                <a href="<?php echo base_url(); ?>index.php/admin/forms"><i class="fa fa-edit fa-fw"></i> Forms</a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/panels">Panels and Wells</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/buttons">Buttons</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/notifications">Notifications</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/typography">Typography</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/icons"> Icons</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/grid">Grid</a>
-                                    </li>
-                                </ul>
-                                <!-- /.nav-second-level -->
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="#">Second Level Item</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Second Level Item</a>
-                                    </li>
-                                    <li>
-                                        <a href="#">Third Level <span class="fa arrow"></span></a>
-                                        <ul class="nav nav-third-level">
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Third Level Item</a>
-                                            </li>
-                                        </ul>
-                                        <!-- /.nav-third-level -->
-                                    </li>
-                                </ul>
-                                <!-- /.nav-second-level -->
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/blank">Blank Page</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/login">Login Page</a>
-                                    </li>
-                                    <li>
-                                        <a href="<?php echo base_url(); ?>index.php/admin/prueba">hola German :D</a>
-                                    </li>
-                                </ul>
-                                <!-- /.nav-second-level -->
-                            </li>
+
+
+
+                            <!-- RESTO DEL MENU -->
+
+
+
+                            
                         </ul>
                     </div>
                     <!-- /.sidebar-collapse -->
